d54519655db4441ddb96345d581aa8c410ba882d
Sergej Domme <sergej.domme@idealo.de>
2022-05-11 17:26:50 +0200

    add different urls to test

--- app/locustfile.py
+++ app/locustfile.py
@@ -5,3 +5,5 @@ class SampleUser(HttpUser):
     @task
     def get_index(self):
         self.client.get("/")
+        self.client.get("/actuator/health/liveness")
+        self.client.get("/actuator/health/readiness")
